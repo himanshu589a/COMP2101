43fd7daf19445e2fa54f5716f4bd3ec00ba80e4b
himanshu <himanshu589a@gmail.com>
2017-10-13 16:28:20 -0700

    made changes

--- bash/lab1/helloworldugly.sh
+++ bash/lab1/helloworldugly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -n "helb wold" |sed "s/I/II/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk
+echo -n "helb wold" |sed -e "s/b/o/g" -e "s/I/II/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk
 '{print $1 "\x20" $2 "\41"}'
